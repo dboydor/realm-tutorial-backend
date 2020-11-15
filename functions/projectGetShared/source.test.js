df985a2ba94af175e76fd27d622298ad62ae8d43
David Boyd <baobei@gmx.com>
2020-11-15 15:05:49 -0800

    - Added mocking to get Jest testing working for Realm function

--- functions/projectGetShared/source.test.js
+++ functions/projectGetShared/source.test.js
@@ -1,14 +1,36 @@
-const {MongoClient} = require('mongodb');
+// --------------------------------
+//  Test for projectGetShared
+// --------------------------------
+const { MongoClient } = require('mongodb');
+const task = require('./source.js');
 
 describe('insert', () => {
   let connection;
   let db;
+  let context = {
+    services: {},
+    user: {_id: '1', name: 'baobei@gmx.com'}
+  }
 
   beforeAll(async () => {
     connection = await MongoClient.connect(global.__MONGO_URI__, {
       useNewUrlParser: true,
     });
     db = await connection.db(global.__MONGO_DB_NAME__);
+
+    global.context = context;
+    global.context.services.get = () => {
+      return {
+        db: () => {
+            return db;
+        }
+      }
+    }
+
+    // const cluster = context.services.get("mongodb-atlas");
+    // const users = cluster.db("tracker").collection("User");
+    // const projects = cluster.db("tracker").collection("Project");
+    // const thisUser = context.user;
   });
 
   afterAll(async () => {
@@ -24,5 +46,7 @@ describe('insert', () => {
 
     const insertedUser = await users.findOne({_id: 'some-user-id'});
     expect(insertedUser).toEqual(mockUser);
+
+    task();
   });
 });
