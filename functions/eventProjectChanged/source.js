df6698d0689dd79db9b466e225c5943985af11b5
David Boyd <baobei@gmx.com>
2020-11-18 15:43:50 -0800

    - Cleanup

--- functions/eventProjectChanged/source.js
+++ functions/eventProjectChanged/source.js
@@ -11,46 +11,23 @@ const task = async function (event) {
   const userId = project.ownerId;
 
   switch (event.operationType) {
-      case "insert":
+      case "INSERT":
         const result = await context.functions.execute("projectAddOwner", projectId, userId);
         if (result && result.error) {
           return result;
         }
         break;
 
-      case "delete":
+      case "DELETE":
         const result = await context.functions.execute("projectRemoveOwner", projectId, userId);
         if (result && result.error) {
           return result;
         }
         break;
 
-      case "replace":
+      case "REPLACE":
         break;
   }
-  /*
-    A Database Trigger will always call a function with a event.
-    Documentation on ChangeEvents: https://docs.mongodb.com/manual/reference/change-events/
-
-    Access the _id of the changed document:
-    const docId = event.documentKey._id;
-
-    Access the latest version of the changed document
-    (with Full Document enabled for Insert, Update, and Replace operations):
-    const fullDocument = event.fullDocument;
-
-    const updateDescription = event.updateDescription;
-
-    See which fields were changed (if any):
-    if (updateDescription) {
-      const updatedFields = updateDescription.updatedFields; // A document containing updated fields
-    }
-
-    See which fields were removed (if any):
-    if (updateDescription) {
-      const removedFields = updateDescription.removedFields; // An array of removed fields
-    }
-  */
 };
 
 // Running as Mongo Realm function
